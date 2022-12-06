b07edbe684b5d391aceda3322debc37817baff79
Colin Maxfield <colin.maxfield@pendo.io>
2022-12-06 10:34:29 -0500

    day 6

---
+++ 2022/src/bin/day_06.rs
@@ -0,0 +1,55 @@
+use aoc2022::read_string;
+use std::collections::HashSet;
+use std::char;
+
+fn search(input: &String, length: usize) -> usize {
+    let characters: Vec<char> = input.chars().collect();
+    let count: usize = input.len();
+    let mut i: usize = 0;
+    while i < (count - length) {
+        let subset = characters[i..i+length].to_vec();
+        let set: HashSet<char> = HashSet::from_iter(subset);
+
+        if set.len() == length {
+            return i + length;
+        }
+        i += 1;
+    }
+
+    0
+}
+
+fn part_1(input: &String) -> usize {
+    search(input, 4)
+}
+
+fn part_2(input: &String) -> usize {
+    search(input, 14)
+}
+
+fn main() {
+    let input = read_string("inputs/day_06.txt");
+
+    println!("Part 1 Results: {}", part_1(&input));
+    println!("Part 2 Results: {}", part_2(&input));
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = read_string("inputs/test/day_06.txt");
+
+        assert_eq!(part_1(&input), 7);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = read_string("inputs/test/day_06.txt");
+
+        assert_eq!(part_2(&input), 19);
+    }
+}
